8cd777027d1313aa085cb1718d7e71b0340f024f
Tim Hinz <tim@chargeover.com>
2022-06-17 11:49:40 -0500

    refactor(backend): enable vue middlewares
    Enables connect-history-api-fallback as a middleware, as well as express static middleware to serve vue files

--- packages/api/src/middleware/index.js
+++ packages/api/src/middleware/index.js
@@ -2,9 +2,30 @@ const cors = require('cors');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const express = require('express');
+const path = require('path');
+const history = require('connect-history-api-fallback');
+
+const staticFileMiddleware = express.static(
+  path.join(__dirname, '../../../app/dist'),
+);
 
 const init = (app) => {
   app.use(morgan('combined'));
+  app.use(staticFileMiddleware);
+  app.use(
+    history({
+      rewrites: [
+        {
+          from: /^\/test\/.*$/,
+          to(context) {
+            return context.parsedUrl.path;
+          },
+        },
+      ],
+    }),
+  );
+
   app.use(bodyParser.json());
   app.use(cors());
   app.use(helmet());
