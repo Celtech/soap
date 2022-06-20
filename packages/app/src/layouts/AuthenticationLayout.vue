bc852146a649024decab81503f032e0ee635e04e
Tim Hinz <tim@chargeover.com>
2022-06-20 15:46:54 -0500

    feat(frontend): tailwind ui login page ui implementation

--- packages/app/src/layouts/AuthenticationLayout.vue
+++ packages/app/src/layouts/AuthenticationLayout.vue
@@ -1,7 +1,19 @@
 <template>
-  <div>
-    TODO AUTH
-    <slot></slot>
+  <div
+    class="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-md">
+      <img
+        class="mx-auto h-12 w-auto"
+        src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+        alt="Workflow" />
+      <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
+        Sign in to your account
+      </h2>
+    </div>
+
+    <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md mx-4">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
