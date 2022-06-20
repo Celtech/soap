e7f7df7d4899de4f3d2b9aae5a198c6bd7f08ad4
Tim Hinz <tim@chargeover.com>
2022-06-20 16:39:50 -0500

    refactor(frontend): move login form to its own component

--- packages/app/src/pages/LoginPage.vue
+++ packages/app/src/pages/LoginPage.vue
@@ -1,63 +1,6 @@
 <template>
   <div class="py-8 px-4 bg-white rounded-lg shadow sm:px-10">
-    <form class="space-y-6" action="#" method="POST">
-      <div>
-        <label for="email" class="block text-sm font-medium text-gray-700">
-          Email address
-        </label>
-        <div class="mt-1">
-          <input
-            id="email"
-            name="email"
-            type="email"
-            autocomplete="email"
-            required=""
-            class="block py-2 px-3 w-full placeholder:text-gray-400 rounded-md border border-gray-300 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 shadow-sm appearance-none sm:text-sm" />
-        </div>
-      </div>
-
-      <div>
-        <label for="password" class="block text-sm font-medium text-gray-700">
-          Password
-        </label>
-        <div class="mt-1">
-          <input
-            id="password"
-            name="password"
-            type="password"
-            autocomplete="current-password"
-            required=""
-            class="block py-2 px-3 w-full placeholder:text-gray-400 rounded-md border border-gray-300 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 shadow-sm appearance-none sm:text-sm" />
-        </div>
-      </div>
-
-      <div class="flex justify-between items-center">
-        <div class="flex items-center">
-          <input
-            id="remember_me"
-            name="remember_me"
-            type="checkbox"
-            class="w-4 h-4 text-indigo-600 rounded border-gray-300 focus:ring-indigo-500" />
-          <label for="remember_me" class="block ml-2 text-sm text-gray-900">
-            Remember me
-          </label>
-        </div>
-
-        <div class="text-sm">
-          <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
-            Forgot your password?
-          </a>
-        </div>
-      </div>
-
-      <div>
-        <button
-          type="submit"
-          class="flex justify-center py-2 px-4 w-full text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 rounded-md border border-transparent focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 shadow-sm">
-          Sign in
-        </button>
-      </div>
-    </form>
+    <LoginForm />
 
     <div class="mt-6">
       <div class="relative">
@@ -127,7 +70,10 @@
 </template>
 
 <script>
+import LoginForm from '../components/authentication/LoginForm.vue';
+
 export default {
   name: 'LoginPage',
+  components: { LoginForm },
 };
 </script>
