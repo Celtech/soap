3ec795bc7ec4a08366211c377ed39f1d8cd95c7c
Tim Hinz <tim@chargeover.com>
2022-06-20 15:36:52 -0500

    feat(frontend): add @tailwind/forms

--- packages/app/tailwind.config.js
+++ packages/app/tailwind.config.js
@@ -1,8 +1,10 @@
+const TailWindForms = require('@tailwindcss/forms');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [TailWindForms],
 };
